6eed5169f16c85c4a850bce59326235da9c48a7f
Adam Barrera <adamjbarrera@utexas.edu>
2021-03-27 08:30:28 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component {
 
     handleModal(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
  
     render() {
@@ -106,7 +106,7 @@ function RenderCampsite({campsite}) {
     ); 
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -120,7 +120,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                             </div>
                         )
                     })}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -167,11 +167,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
+                    <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
-                    />
+                    />     
                 </div>
             </div>
         );
